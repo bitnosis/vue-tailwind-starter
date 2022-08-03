03a42615a57ed8c7e9c004c309c6709d0af6d063
Kylie Koopman <kylie.koopman@fullscript.com>
2022-08-02 20:07:13 -0700

    wip

---
+++ docs/precache-manifest.ab76323b69c061b8575ef94296ab1491.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9931747adcf7eb3c8b43",
+    "url": "/vue-tailwind-starter/css/app.92cb7079.css"
+  },
+  {
+    "revision": "eb56cc02b069487300d1",
+    "url": "/vue-tailwind-starter/css/chunk-vendors.ea621d90.css"
+  },
+  {
+    "revision": "6b03b6d2d963afd943f7952ad172114c",
+    "url": "/vue-tailwind-starter/img/logo.6b03b6d2.png"
+  },
+  {
+    "revision": "60f36bc13117a0f92c269eae6c8a622d",
+    "url": "/vue-tailwind-starter/index.html"
+  },
+  {
+    "revision": "9931747adcf7eb3c8b43",
+    "url": "/vue-tailwind-starter/js/app.e9ad6ee0.js"
+  },
+  {
+    "revision": "eb56cc02b069487300d1",
+    "url": "/vue-tailwind-starter/js/chunk-vendors.b00fda58.js"
+  },
+  {
+    "revision": "8876e4945b560b2750273333d912b39c",
+    "url": "/vue-tailwind-starter/manifest.json"
+  }
+]);
