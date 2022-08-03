a0ad867dbd1bdbc41b7a016228bf98b6ea2cdc87
Kylie Koopman <kylie.koopman@fullscript.com>
2022-08-02 21:26:18 -0700

    more fake users

--- src/store/index.js
+++ src/store/index.js
@@ -8,7 +8,7 @@ Vue.use(Vuex);
 export default new Vuex.Store({
   state: {
     page: "experiments",
-    users: generateFakeUsers(500),
+    users: generateFakeUsers(1500),
     experiments: [
       {
         id: generateUUID(),
@@ -40,31 +40,6 @@ export default new Vuex.Store({
     getExperiments: state => state.experiments
   },
   mutations: {
-    /* GET_EXPERIMENT(state, experiment){
-      state.newexperiment = experiment
-    },
-    ADD_EXPERIMENT(state){
-      state.experiments.push({
-        body: state.newexperiment,
-        completed: false
-      })
-    },
-    EDIT_EXPERIMENT(state, experiment){
-       var experiments = state.experiments
-       experiments.splice(experiments.indexOf(experiment), 1)
-       state.experiments = experiments
-       state.newexperiment = experiment.body
-    },
-    REMOVE_EXPERIMENT(state, experiment){
-       var experiments = state.experiments
-       experiments.splice(experiments.indexOf(experiment), 1)
-    },
-    COMPLETE_EXPERIMENT(state, experiment){
-      experiment.completed = !experiment.completed
-    },
-    CLEAR_EXPERIMENT(state){
-      state.newexperiment = ''
-    }*/
     SET_PAGE(state, page) {
       state.page = page;
     },
@@ -95,23 +70,5 @@ export default new Vuex.Store({
     setUsers({ commit }, users) {
       commit("SET_USERS", users);
     }
-    /*getExperiment({commit}, experiment){
-      commit(‘GET_EXPERIMENT’, experiment)
-    },
-    addexperiment({commit}){
-      commit(‘ADD_EXPERIMENT’)
-    },
-    editexperiment({commit}, experiment){
-      commit(‘EDIT_EXPERIMENT’, experiment)
-    },
-    removeexperiment({commit}, experiment){
-      commit(‘REMOVE_EXPERIMENT’, experiment)
-    },
-    completeexperiment({commit}, experiment){
-     commit(‘COMPLETE_EXPERIMENT’, experiment)
-    },
-    clearexperiment({commit}){
-      commit('CLEAR_EXPERIMENT')
-    }*/
   }
 });
