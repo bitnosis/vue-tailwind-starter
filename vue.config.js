5f026e770d1507795a76e8a2b0f724daa9ec8298
Kylie Koopman <kylie.koopman@fullscript.com>
2022-08-02 21:41:58 -0700

    fix build

--- vue.config.js
+++ vue.config.js
@@ -1,21 +1,21 @@
 // https://cli.vuejs.org/config/#configuration-reference
-const config = require('./package.json');
+const config = require("./package.json");
 
 process.env.VUE_APP_VERSION = config.version;
 
-const repoName = config.repository.url.match(/\/\w+\/(.*?)\.git$/i)[1];
+// const repoName = config.repository.url.match(/\/\w+\/(.*?)\.git$/i)[1];
 
 // pull in tailwind theme colors for the tile colors
-const { emerald } = require('tailwindcss/colors');
+const { emerald } = require("tailwindcss/colors");
 
 module.exports = {
   // `docs` is a special folder that can be served via Github Pages without using a `gh-pages` branch
-  outputDir: 'docs',
-  publicPath: process.env.NODE_ENV === 'production' ? `/${repoName}/` : '/',
+  outputDir: "docs",
+  publicPath: process.env.NODE_ENV === "production" ? "/" : "/",
   pwa: {
-    themeColor: emerald['600'],
-    msTileColor: emerald['600'],
-    appleMobileWebAppCapable: 'yes',
-    appleMobileWebAppStatusBarStyle: 'black',
+    themeColor: emerald["600"],
+    msTileColor: emerald["600"],
+    appleMobileWebAppCapable: "yes",
+    appleMobileWebAppStatusBarStyle: "black"
   }
 };
